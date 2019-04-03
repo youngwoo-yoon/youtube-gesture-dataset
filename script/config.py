842f782fa7a7d09cba58bc60c91c1cc4b9f07ddf
Youngwoo Yoon <zeroyy@gmail.com>
2019-04-03 10:26:07 +0900

    Initial commit

---
+++ script/config.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+
+class Config:
+    DEVELOPER_KEY = ""  # your youtube developer id
+    OPENPOSE_BASE_DIR = "/mnt/work/work/openpose/"
+    OPENPOSE_BIN_PATH = "build/examples/openpose/openpose.bin"
+
+
+class TEDConfig(Config):
+    YOUTUBE_CHANNEL_ID = "UCAuUUnT6oDeKwE6v1NGQxug"
+    WORK_PATH = '/mnt/work/work/Youtube_Dataset'
+    CLIP_PATH = WORK_PATH + "/clip_ted"
+    VIDEO_PATH = WORK_PATH + "/videos_ted"
+    SKELETON_PATH = WORK_PATH + "/skeleton_ted"
+    SUBTITLE_PATH = VIDEO_PATH
+    OUTPUT_PATH = WORK_PATH + "/output"
+    VIDEO_SEARCH_START_DATE = datetime(2011, 3, 1, 0, 0, 0)
+    LANG = 'en'
+    SUBTITLE_TYPE = 'gentle'
+    FILTER_OPTION = {"threshold": 100}
+
+
+class LaughConfig(Config):
+    YOUTUBE_CHANNEL_ID = "UCxyCzPY2pjAjrxoSYclpuLg"
+    WORK_PATH = '/mnt/work/work/Youtube_Dataset'
+    CLIP_PATH = WORK_PATH + "/clip_laugh"
+    VIDEO_PATH = WORK_PATH + "/videos_laugh"
+    SKELETON_PATH = WORK_PATH + "/skeleton_laugh"
+    SUBTITLE_PATH = VIDEO_PATH
+    OUTPUT_PATH = WORK_PATH + "/output"
+    VIDEO_SEARCH_START_DATE = datetime(2010, 5, 1, 0, 0, 0)
+    LANG = 'en'
+    SUBTITLE_TYPE = 'auto'
+    FILTER_OPTION = {"threshold": 50}
+
+
+# SET THIS
+my_config = TEDConfig
